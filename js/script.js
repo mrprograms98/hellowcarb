b56127ac919841a6fe75fb64ea07e0b6afde5317
mr-programs <m.rashid@live.nl>
2025-12-31 20:07:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,3 +88,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
